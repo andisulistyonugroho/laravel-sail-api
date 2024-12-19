bacbfc58fe5bc7855542dcf474651926095daa66
andisulistyonugroho <andisulistyonugroho@gmail.com>
2024-12-19 19:51:31 +0700

    fix: refresh migrate add field active

--- database/migrations/2024_12_19_115828_create_users_table.php
+++ database/migrations/2024_12_19_115828_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('name');
+            $table->boolean('active')->default(true);
             $table->timestamp('created_at');
         });
     }
